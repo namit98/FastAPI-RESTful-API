7ca8ecbe2e97f3634eec94334d5226553302b6d7
Namit <43697251+namit98@users.noreply.github.com>
2020-09-26 18:17:09 +0530

    first commit

---
+++ server/app.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from server.routes.student import router as StudentRouter
+
+app = FastAPI()
+app.include_router(StudentRouter, tags=["Student"], prefix="/student")
+
+@app.get("/", tags=["Root"])
+async def read_root():
+    return {"message": "Welcome to this awesome app! by Namit"}
